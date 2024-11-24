02e50de528ab48d9551c1c1b5c61d3bfabdacd62
Md Habibullah <smrakib333@gmail.com>
2024-11-25 02:53:00 +0600

    initial  commit

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import app from "./app";
+import mongoose from "mongoose";
+
+// async function main() {
+//   await mongoose.connect(process.env.DATABASE_URL);
+// }
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`);
+});
